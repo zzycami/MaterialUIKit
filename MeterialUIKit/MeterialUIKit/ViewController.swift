84a4afa8adfcdfb41611aa7473221b3d15fb39f4
zzycami <zzycami@foxmail.com>
2014-12-30 12:42:43 +0800

    init

---
+++ MeterialUIKit/MeterialUIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeterialUIKit
+//
+//  Created by damingdan on 14/12/30.
+//  Copyright (c) 2014年 com.metalmind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
